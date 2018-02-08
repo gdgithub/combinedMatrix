b7d114ffe384dbc20c885d4c868c0a54625cc7b8
Ivan Gil Cruz <ivangilcruz@MacBook-Pro-de-MacBookPro.local>
2018-02-08 13:30:45 -0400

    Stable

---
+++ combinedMatrix.py
@@ -0,0 +1,10 @@
+from matrices import genTotallyPositiveMatriz, genUpperBiDiagonalMatrix, genLowerBiDiagonalMatrix, genDiagonalMatrix, combinedMatrix, isaTotallyPositiveMatrix, getSubMatrices
+
+def help():
+    print "1) getSubmatrices"
+    print "2) isaTotallyPositiveMatrix"
+    print "3) combinedMatrix"
+    print "4) genDiagonalMatrix"
+    print "5) genLowerBiDiagonalMatrix"
+    print "6) genUpperBiDiagonalMatrix"
+    print "7) genTotallyPositiveMatriz"
